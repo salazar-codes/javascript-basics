5a4fe56f8c8e56b05eaa951106614e7cea487ddf
Jimmy Salazar <jimmyr.salazar@gmail.com>
2023-12-25 17:50:16 -0500

    underscore library added

---
+++ 02-blackjack/assets/js/juego.js
@@ -0,0 +1,30 @@
+/**
+ *  2C = Two of Clubs
+ *  2D = Two of Diamonds
+ *  2H = Two of Hearts
+ *  2S = Two of Spades
+ */
+
+let deck = [];
+const tipos = ['C','D','H','S']; // tipos de cartas
+const especiales = ['A','J','Q','K']; // cartas especiales
+
+const crearDeck = () => {
+    for (let i = 2; i < 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+
+    console.log( deck );
+    deck = _.shuffle( deck )
+    console.log( deck );
+}
+
+crearDeck();
