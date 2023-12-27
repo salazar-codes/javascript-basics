d9da629c9a3fef64e0ff18706e3003885aa1db53
Jimmy Salazar <jimmy.salazar@tektonlabs.com>
2023-12-27 16:48:20 -0500

    static props and methods added

--- 07-clases/class.js
+++ 07-clases/class.js
@@ -1,14 +1,37 @@
 
 class Persona {
 
+    static _conteo = 0;
+
+    static get conteo(){
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje(){
+        console.log('Mensaje custom de la clase');
+    }
+
     nombre = '';
     codigo = '';
     frase = 'ssss';
+    comida = '';
+    // No es necesario escribir las variables aquí ya que el constructor las crea
 
     constructor(nombre = 'Sin nombre', codigo= 'Sin codigo', frase = 'Sin frase') {
         this.codigo = codigo;
         this.nombre = nombre;
         this.frase = frase;
+        Persona._conteo++;
+    }
+
+    // El set debería recibir sólo un argumento
+    // no pueden tener el mismo nombre del atributo
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita es ${ this.comida }`;
     }
 
     quienSoy(){
@@ -21,8 +44,19 @@ class Persona {
 }
 
 const spiderman = new Persona("peter parket","spiderman","Hey tú!");
-console.log(spiderman.frase);
+const spiderman2 = new Persona("peter parket","spiderman","Hey tú!");
+console.log(spiderman);
 
 spiderman.quienSoy();
-spiderman.otroMetodo();
 
+// No es una función, si no que se trata como una propiedad
+spiderman.setComidaFavorita = 'Arroz con pollo';
+console.log(spiderman.getComidaFavorita);
+
+console.log('Conteo estático: ' + Persona.conteo);
+
+Persona.mensaje();
+
+// En JS se pueden definir propiedades externas a la clase
+Persona.nuevapropiedad = 'prop externa';
+console.log(Persona.nuevapropiedad);
