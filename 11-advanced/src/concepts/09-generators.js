4eb46fee9c044e7ff1f0a031a236a13c2c0c7372
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-01-04 22:02:00 -0500

    generator function example

--- 11-advanced/src/concepts/09-generators.js
+++ 11-advanced/src/concepts/09-generators.js
@@ -4,14 +4,29 @@
  */
 export const generatorFunctionsComponent = ( element ) => {    
     
-    const myGenerator = myFirstGeneratorFunction();
+    // const myGenerator = myFirstGeneratorFunction();
 
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
+    const genId = idGenerator();
+
+    console.log(genId.next());
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+    const renderButton = () => {
+        const { value } = genId.next();
+        button.innerText = `Click ${value}`;
+    }
+
+    button.addEventListener('click', renderButton);
+}
+
+function* idGenerator() {
+    let currentId = 0;
+    while(true){
+        yield ++currentId; //Devuelve el primer id antes de incrementar
+    }
 }
 
 // También se puede usar con await
