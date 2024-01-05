390597b03add5dbf2e9256c250789eaabc0a9f38
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-01-04 20:04:19 -0500

    adds callback error management

--- 11-advanced/src/concepts/02-callbacks.js
+++ 11-advanced/src/concepts/02-callbacks.js
@@ -6,21 +6,29 @@ import { heroes } from "../data/heroes";
  */
 export const callbacksComponent = ( element ) => {    
     
-    const id = '5d86371f233c9f2425f16916';
-    findHero(id, (hero) => {
-        element.innerHTML = hero.name;
-    });
+    const id = '5d86371f97c29d020f1e1f6d';
 
+    findHero(id, (error, hero) => {
+        if( error ) {
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero?.name || 'No hay heroe';
+    });
 }
 
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object) => void } callback 
+ * @param { (error: String|Null, hero: Object) => void } callback 
  */
 const findHero = ( id, callback ) => {
 
     const hero = heroes.find( hero => hero.id === id );
 
-    callback( hero );
+    if( !hero ) {
+        callback(`Hero with id ${id} not found.`);
+        return;
+    }
+    callback( null, hero );
 }
